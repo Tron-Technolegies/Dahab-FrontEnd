0636c5743863c4417810213feede3a54b2fee718
clawlynx <clawlynx77@gmail.com>
2025-08-14 14:12:33 +0530

    corrected dates and time to UAE format and made terms section

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -17,7 +17,7 @@ export default function FormInput({
       <div className="flex items-center">
         <input
           type={type}
-          className="w-full py-1 px-3 rounded-lg bg-white border border-[#0B578E] outline-none text-gray-900 h-11"
+          className="w-full py-1 px-3 rounded-lg bg-white border  outline-none text-gray-900 h-11"
           placeholder={placeholder}
           value={value}
           onChange={onchange}
